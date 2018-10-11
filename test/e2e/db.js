3cf998e5534c90fa1d484d8f5793898747ef250f
Sophie Graham <sophiergraham1@gmail.com>
2018-10-11 11:32:37 -0700

    added db test

---
+++ test/e2e/db.js
@@ -0,0 +1,16 @@
+const connect = require('../../lib/util/connect');
+connect('mongodb://localhost:27017/tours_test');
+const mongoose = require('mongoose');
+
+afterAll(() => {
+    return mongoose.disconnect();
+});
+
+module.exports = {
+    dropCollection(name) {
+        return mongoose.connection.dropCollection(name)
+            .catch(err => {
+                if(err.codeName !== 'NamespaceNotFound') throw err;
+            });
+    }
+};
