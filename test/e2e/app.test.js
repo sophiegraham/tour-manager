baa36ea971a7999e7722021310a63603e0ca3a32
Sophie Graham <sophiergraham1@gmail.com>
2018-10-19 16:49:29 -0700

    passing post test and model test

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -1,57 +1,45 @@
-// const { dropCollection } = require('./db');
-// const request = require('supertest');
-// const app = require('../../lib/app');
-// const Chance = require('chance');
-// const chance = new Chance();
+const { dropCollection } = require('./db');
+const request = require('supertest');
+const app = require('../../lib/app');
 
-// describe('tour pub/sub API', () => {
-//     let tours = Array.apply(null, { length: 30 }).map(() => {
-//         return {
-//             tourID: chance.guid({ version: 4 }),
-//             title: 'purchase',
-//             activities: ,
-//             launchDate: ,
-//             stops:
-//         };
-//     });
-//     let createdTours;
+describe('tour pub/sub API', () => {
+    let tours = Array.apply(null, { length: 30 }).map(() => {
+        return {
+            title: 'best circus',
+            activities: ['juggling', 'trapeze', 'animal abuse'],
+            launchDate: new Date(),
+            stops:[]
+        };
+    });
+    let createdTours;
 
-//     const createTour = tour => {
-//         return request(app)
-//             .post('/api/tours')
-//             .send(tour)
-//             .then(res => res.body);
-//     };
+    const createTour = tour => {
+        return request(app)
+            .post('/api/tours')
+            .send(tour)
+            .then(res => res.body);
+    };
 
-//     beforeEach(() => {
-//         return dropCollection('tours');
-//     });
+    beforeEach(() => {
+        return dropCollection('tours');
+    });
 
-//     beforeEach(() => {
-//         return Promise.all(tours.map(createTour)).then(toursRes => {
-//             createdTours = toursRes;
-//         });
-//     });
+    beforeEach(() => {
+        return Promise.all(tours.map(createTour)).then(toursRes => {
+            createdTours = toursRes;
+        });
+    });
 
-//     it('creates a tour on post', () => {
-//         return request(app)
-//             .post('/api/tours')
-//             .send({
-//                 customerId: '1234',
-//                 title: 'purchase',
-//                 activities: '5678',
-//                 launchDate: ,
-//                 stops
-//             })
-//             .then(res => {
-//                 expect(res.body).toEqual({
-//                     _id: expect.any(String),
-//                     __v: expect.any(Number),
-//                     title: 'purchase',
-//                     activities: '5678',
-//                     launchDate: ,
-//                     stops
-//                 });
-//             });
-//     });
-
+    it.skip('creates a tour on post', () => {
+        return request(app)
+            .post('/api/tours')
+            .send(createdTours[0])
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    __v: expect.any(Number),
+                    ...createdTours[0]
+                });
+            });
+    });
+});
--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -1,25 +1,37 @@
 const { getErrors } = require('./helpers');
-const Tour = require('../../lib/models/');
-const Chance = require('chance');
-const chance = new Chance();
+const Tour = require('../../lib/models/Tour');
 
 describe('Tours model', () => {
     it('validates a good model', () => {
+        const stop = { 
+            location: {
+                city: 'portland',
+                state: 'oregon',
+                zip: '97205'
+            },
+            weather: {
+                temperature: '70',
+                condition: 'raining'
+            },
+            attendance: 250
+        };
+
         const data = {
-            title, activities, lanchDate, stops 
+            title: 'best circus',
+            activities: ['juggling', 'trapeze', 'animal abuse'],
+            launchDate: new Date(),
+            stops:[stop] 
         };
 
         const tour = new Tour(data);
         const jsonTour = tour.toJSON();
-        expect(jsonTour).toEqual({ ...data, _id: expect.any(Object) });
+        expect(jsonTour).toEqual({ ...data, _id: expect.any(Object), stops:[{ ...stop, _id:expect.any(Object) }] });
     });
 
-    it.skip('event type is required', () => {
-        const tour = new Tour({
-            //idk what to put here
-        });
+    it('event type is required', () => {
+        const tour = new Tour({});
 
         const errors = getErrors(tour.validateSync(), 1);
-        expect(errors.type.properties.message).toEqual('Path `type` is required.');
+        expect(errors.title.kind).toEqual('required');
     });
 });
